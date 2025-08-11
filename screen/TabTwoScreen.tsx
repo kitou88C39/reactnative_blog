f929bc842e260384bb69f008e621d71bc28f6a25
kitoumasaomi <kanfutrooper@gmail.com>
2025-08-11 14:31:47 +0900

    screen/TabTwoScreen.tsxを作成する

---
+++ screen/TabTwoScreen.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+import { createTable, insert, select } from '../helpers/sqlite';
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  createTable(); //diariesテーブルを作成
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='/screens/TabOneScreen.tsx' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  list: {
+    flex: 1,
+  },
+  todoItem: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+  },
+});
